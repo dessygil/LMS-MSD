6f34681f3a1ce3ec33bc4ef8243b6a75eb7d9df5
dessygil <desmondgilmour@gmail.com>
2023-04-04 14:07:00 -0400

    connected to planetscale, .env not included

--- API/API/asgi.py
+++ API/API/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for LMSAPI project.
+ASGI config for API project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LMSAPI.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "API.settings")
 
 application = get_asgi_application()
