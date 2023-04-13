a4754041139eb88d2bdd2181cbefc4f5fd02af8f
dessygil <desmondgilmour@gmail.com>
2023-04-13 15:11:24 -0400

    changed the tutorial to the most recent django one

--- API/user/urls.py
+++ API/user/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('public', views.public),
-    path('private', views.private),
-    path('register', views.RegisterView.as_view()),
+    path('api/public', views.public),
+    path('api/private', views.private),
+    path('api/private-scoped', views.private_scoped)
 ]
--- API/user/views.py
+++ API/user/views.py
@@ -1,61 +1,37 @@
-from functools import wraps
-from .serializers import UserSerializer
-from rest_framework.views import APIView
-from rest_framework.response import Response
-import jwt
+from authlib.integrations.django_oauth2 import ResourceProtector
 from django.http import JsonResponse
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
+from . import validator
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+require_auth = ResourceProtector()
+validator = validator.Auth0JWTBearerTokenValidator(
+    os.getenv("JWT_ISSUER"),
+    os.getenv("JWT_AUDIENCE"),
+)
+require_auth.register_token_validator(validator)
 
 
-# auth0 authorization using access token
-def get_token_auth_header(request):
-    """Obtains the Access Token from the Authorization Header
-    """
-    auth = request.META.get("HTTP_AUTHORIZATION", None)
-    parts = auth.split()
-    token = parts[1]
-
-    return token
-
-def requires_scope(required_scope):
-    """Determines if the required scope is present in the Access Token
-    Args:
-        required_scope (str): The scope required to access the resource
-    """
-    def require_scope(f):
-        @wraps(f)
-        def decorated(*args, **kwargs):
-            token = get_token_auth_header(args[0])
-            decoded = jwt.decode(token, verify=False)
-            if decoded.get("scope"):
-                token_scopes = decoded["scope"].split()
-                for token_scope in token_scopes:
-                    if token_scope == required_scope:
-                        return f(*args, **kwargs)
-            response = JsonResponse({'message': 'You don\'t have access to this resource'})
-            response.status_code = 403
-            return response
-        return decorated
-    return require_scope
-
-class RegisterView(APIView):
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            if user:
-                return Response(serializer.data, status=201)
-        return JsonResponse(serializer.errors)
-
-
-# Test endpoints
-@api_view(['GET'])
-@permission_classes([AllowAny])
 def public(request):
-    return JsonResponse({'message': 'Hello from a public endpoint! You don\'t need to be authenticated to see this.'})
+    """No access token required to access this route
+    """
+    response = "Hello from a public endpoint! You don't need to be authenticated to see this."
+    return JsonResponse(dict(message=response))
 
 
-@api_view(['GET'])
+@require_auth(None)
 def private(request):
-    return JsonResponse({'message': 'Hello from a private endpoint! You need to be authenticated to see this.'})
+    """A valid access token is required to access this route
+    """
+    response = "Hello from a private endpoint! You need to be authenticated to see this."
+    return JsonResponse(dict(message=response))
+
+
+@require_auth("read:messages")
+def private_scoped(request):
+    """A valid access token and an appropriate scope are required to access this route
+    """
+    response = "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this."
+    return JsonResponse(dict(message=response))
